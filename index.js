5e46a83da071d5ea7532d188f4c9400364670ab4
ddaaggeett <david.g.daggett@gmail.com>
2018-07-12 18:00:00 -0400

    getScreenshot()

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const { spawn } = require('child_process')
+const { spawn, exec } = require('child_process')
 
-var adb_devices = []
+get_adb_device_list().then(device_list => {
+    console.log('adb devices available:\n' + device_list + '\n')
+    startCameras(device_list)
+    getScreenshot(device_list[0]).then(() => { // TODO: prompt by user
+        pullScreenShot(device_list[0])
+    })
+})
+
+function getScreenshot(device) {
+    return new Promise((resolve,reject) => {
+        exec('adb shell screencap -p /sdcard/screen.png', (err,stdout,stdin) => {
+            if(err) process.exit()
+            resolve()
+        })
+    })
+}
+
+function pullScreenShot(device) {
+    return new Promise((resolve,reject) => {
+        exec('adb pull /sdcard/screen.png ./screenshots', (err,stdout,stdin) => {
+            if(err) process.exit()
+            resolve()
+        })
+    })
+}
 
 function get_adb_device_list() {
-
     return new Promise((resolve, reject) => {
-
         spawn('adb', ['devices']).stdout.on('data', function(data) {
             var devices = []
             var arrayOfStrings = data.toString().split('\n')
@@ -20,14 +42,7 @@ function get_adb_device_list() {
             }
         })
     })
-
 }
-
-get_adb_device_list()
-.then(device_list => {
-    console.log('adb devices available:\n' + device_list + '\n')
-    startCameras(device_list)
-})
 
 function startCameras(devices) {
     devices.forEach( device => {
@@ -42,7 +57,7 @@ function startVideoCamera(device) {
         .stdout.on('data', function(data) {
             setTimeout(() => {
                 resolve(device)
-            }, 1000) // TODO: don't rely on setTimeout. imporoe confirmation camera is up and running
+            }, 1000) // TODO: don't rely on setTimeout. improve confirmation camera is up and running
         })
     })
 }
