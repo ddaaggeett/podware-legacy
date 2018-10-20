2e854ee4d9d9427bbbd43c97be6e2df9f36f1fdd
ddaaggeett <david.g.daggett@gmail.com>
2018-10-20 07:58:04 -0400

    pull video file

--- src/video/index.js
+++ src/video/index.js
@@ -1,4 +1,10 @@
 import {
+    exec,
+} from 'child_process'
+import {
+    recordingsDir,
+} from '../../config'
+import {
     socketPort,
     socketPort_local,
 } from '../../config'
@@ -10,6 +16,12 @@ var videoStop
 
 io_remote.on('connect', (socket) => {
     console.log('connected to camera')
+
+    socket.on('videoReadyToPull', (file) => {
+        exec('adb pull ' + file + ' ' + recordingsDir, (err, stdout, stdin) => {
+            if(err) console.log(err)
+        })
+    })
 
     videoStart = () => {
         socket.emit('startRecording')
